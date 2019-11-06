f1ca259d704841e3d071e27390fb26ff0c9bb38e
ruthtech <leeruths@gmail.com>
2019-11-06 00:25:04 -0500

    Fix SQL syntax errors

--- db/cms.sql
+++ db/cms.sql
@@ -4,22 +4,25 @@ CREATE database cms;
 
 USE cms;
 
-CREATE TABLE department {
+CREATE TABLE department (
   id INTEGER NOT NULL auto_increment PRIMARY KEY,
   name VARCHAR(30)
-}
+);
 
-CREATE TABLE role {
+CREATE TABLE role (
   id INTEGER NOT NULL auto_increment PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
+  department_id INTEGER,
   FOREIGN KEY (department_id) REFERENCES department(id)
-}
+);
 
 CREATE TABLE employee (
   id INTEGER NOT NULL auto_increment PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
+  role_id INTEGER,
+  manager_id INTEGER,
   FOREIGN KEY (role_id) REFERENCES role(id),
   FOREIGN KEY (manager_id) REFERENCES role(id)
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,32 +1,61 @@
+DROP DATABASE IF EXISTS cms;
+
+CREATE database cms;
+
+USE cms;
+
+CREATE TABLE department (
+  id INTEGER NOT NULL auto_increment PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+CREATE TABLE role (
+  id INTEGER NOT NULL auto_increment PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INTEGER,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INTEGER NOT NULL auto_increment PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INTEGER,
+  manager_id INTEGER,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES role(id)
+);
+
+
 INSERT into department (name) VALUES ("sales");
 INSERT into department (name) VALUES ("IT");
 INSERT into department (name) VALUES ("Bob");
 INSERT into department (name) VALUES ("HR");
 
-INSERT into role (title, salary, department_id) VALUES ("Sales Manager", 100000, 0);
-INSERT into role (title, salary, department_id) VALUES ("Sales person", 50000, 0);
-INSERT into role (title, salary, department_id) VALUES ("IT Manager", 100000, 1);
-INSERT into role (title, salary, department_id) VALUES ("Engineer", 900000, 1);
-INSERT into role (title, salary, department_id) VALUES ("Bob Manager", 100000, 2);
-INSERT into role (title, salary, department_id) VALUES ("Artist Bob", 30000, 2);
-INSERT into role (title, salary, department_id) VALUES ("Musician Bob", 30000, 2);
-INSERT into role (title, salary, department_id) VALUES ("Comedian Bob", 30000, 2);
-INSERT into role (title, salary, department_id) VALUES ("Counselor", 80000, 3);
+INSERT into role (title, salary, department_id) VALUES ("Sales Manager", 100000, 1);
+INSERT into role (title, salary, department_id) VALUES ("Sales person", 50000, 1);
+INSERT into role (title, salary, department_id) VALUES ("IT Manager", 100000, 2);
+INSERT into role (title, salary, department_id) VALUES ("Engineer", 900000, 2);
+INSERT into role (title, salary, department_id) VALUES ("Bob Manager", 100000, 3);
+INSERT into role (title, salary, department_id) VALUES ("Artist Bob", 30000, 3);
+INSERT into role (title, salary, department_id) VALUES ("Musician Bob", 30000, 3);
+INSERT into role (title, salary, department_id) VALUES ("Comedian Bob", 30000, 3);
+INSERT into role (title, salary, department_id) VALUES ("Counselor", 80000, 4);
 
+INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("John H.", "Patterson", 1, null);
+INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Mary Kay", "Ash", 2, 1);
+INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Dale", "Carnegie", 2, 1);
 
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("John H.", "Patterson", 0, null);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Mary Kay", "Ash", 1, 0);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Dale", "Carnegie", 1, 0);
+INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Montgomery", "Scott", 3, null);
+INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Angus", "MacGyver", 4, 3);
+INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Kaylee", "Frye", 4, 3);
+INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Cyrus", "Smith", 4, 3);
 
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Montgomery", "Scott", 2, null);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Angus", "MacGyver", 3, 2);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Kaylee", "Frye", 3, 2);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Cyrus", "Smith", 3, 2);
+INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Bob", "The Minion", 5, null);
+INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Bob", "Ross", 6, 5);
+INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Bob", "Marley", 7, 5);
+INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Bob", "Dylan", 7, 5);
+INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Bob", "Hope", 8, 5);
 
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Bob", "The Minion", 4, null);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Bob", "Marley", 5, 4);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Bob", "Dylan", 5, 4);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Bob", "Ross", 6, 4);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Bob", "Hope", 7, 4);
-
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Deanna", "Troi", 8, null);
+INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Deanna", "Troi", 9, null);
