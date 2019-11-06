74dccacc61838c25345c567bfc9009df2518ec28
ruthtech <leeruths@gmail.com>
2019-11-06 16:42:06 -0500

    Undo changes that was accidentally added to the wrong file.

--- db/seed.sql
+++ db/seed.sql
@@ -1,34 +1,4 @@
-DROP DATABASE IF EXISTS cms;
-
-CREATE database cms;
-
-USE cms;
-
-CREATE TABLE department (
-  id INTEGER NOT NULL auto_increment PRIMARY KEY,
-  name VARCHAR(30)
-);
-
-CREATE TABLE role (
-  id INTEGER NOT NULL auto_increment PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INTEGER,
-  FOREIGN KEY (department_id) REFERENCES department(id)
-);
-
-CREATE TABLE employee (
-  id INTEGER NOT NULL auto_increment PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INTEGER,
-  manager_id INTEGER,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES role(id)
-);
-
-
-INSERT into department (name) VALUES ("sales");
+INSERT into department (name) VALUES ("Sales");
 INSERT into department (name) VALUES ("IT");
 INSERT into department (name) VALUES ("Bob");
 INSERT into department (name) VALUES ("HR");
